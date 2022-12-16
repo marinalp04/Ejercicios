ec3f105984fa71bebe7b02e38fa8027d470bdf63
marinalp04 <arriagamarina4@gmail.com>
2022-12-16 17:56:15 +0100

    ejercicios

--- Prueba/ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿string nombre;
-Console.WriteLine("Como te llamas?");
-nombre = Console.ReadLine();
-char comilla = ('"');
-Console.Write(comilla + "Hola,");
-Console.WriteLine(nombre + comilla);
-Console.ReadLine();
--- Prueba/ejercicio8/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("introduce un numero entero");
-int num = Convert.ToInt32(Console.ReadLine());
-int decenas, unidades, numerofinal;
-decenas = num / 10;
-unidades = num % 10;
-numerofinal = (unidades * 10) + decenas;
-Console.WriteLine(numerofinal);
-Console.ReadLine();
